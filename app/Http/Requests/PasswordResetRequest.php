ad70c9a7130e0ecee6565fd8aeba2644c3253c4b
Ari Umboro Seno <ariumboroseno@gmail.com>
2023-11-25 21:00:27 +0700

    update request

--- app/Http/Requests/PasswordResetRequest.php
+++ app/Http/Requests/PasswordResetRequest.php
@@ -25,7 +25,7 @@ class PasswordResetRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|email|exists:users',
+            'email' => 'required|email|exists:users,email',
         ];
     }
 }
